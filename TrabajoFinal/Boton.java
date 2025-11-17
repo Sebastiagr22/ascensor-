5b39ee81e47bd218acdf12051cf50551820df910
Sebastiagr22 <sebastian.giraldore@campusucc.edu.co>
2025-11-17 05:51:11 +0000

    boton

---
+++ TrabajoFinal/Boton.java
@@ -0,0 +1,23 @@
+public class Boton {
+    protected boolean iluminado = false;
+    protected boolean presionado = false;
+
+    public void presionar (){
+        this.presionado = true;
+        this.ilumunar ();
+    }
+    public void ilumunar(){
+        this.iluminado = true;
+        System.out.println("Boton iluminado");
+    }
+    public void apagar(){
+        this.iluminado = false;
+        System.out.println("boton apagado");
+    }
+    public boolean isPresionado (){
+        return presionado;
+    }
+     public boolean isIluminado (){
+        return iluminado;
+    }
+}
