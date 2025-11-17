3e1c0221b2cc1df8dd6463e3d70a50589d557597
Sebastiagr22 <sebastian.giraldore@campusucc.edu.co>
2025-11-17 06:11:56 +0000

    cambios

---
+++ TrabajoFinal/BotonPiso.java
@@ -0,0 +1,15 @@
+public class BotonPiso extends Boton {
+    private int piso;
+    private boolean subir; 
+
+    public BotonPiso(int piso, boolean subir);{
+        super();
+        this.piso = piso;
+        this.subir =  subir;
+    }
+
+    @Override
+    
+
+
+}
