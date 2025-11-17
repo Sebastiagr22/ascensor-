6cc2d673f33d025279bd55e06a2f36eb7443b5d0
Sebastiagr22 <sebastian.giraldore@campusucc.edu.co>
2025-11-17 06:19:53 +0000

    cambios

--- TrabajoFinal/BotonPiso.java
+++ TrabajoFinal/BotonPiso.java
@@ -2,14 +2,21 @@ public class BotonPiso extends Boton {
     private int piso;
     private boolean subir; 
 
-    public BotonPiso(int piso, boolean subir);{
-        super();
+    public BotonPiso(int piso, boolean subir){
         this.piso = piso;
         this.subir =  subir;
     }
-
     @Override
-    
+public void presionar() {
+    super.presionar();
+    String direccion;
+    if (subir) {
+        direccion = "Subir";
+    } else {
+        direccion = "Bajar";
+    }
+    System.out.println("Solicitud en piso " + piso + " dirección: " + direccion);
+}
 
 
 }
